8f2bd65324c6c2b4a7632d5325527719ebc30f12
prabhat-ranjan50 <91838417+prabhat-ranjan50@users.noreply.github.com>
2021-11-01 19:42:54 +0530

    Add files via upload

---
+++ Prabhat3.js
@@ -0,0 +1,28 @@
+
+var x;
+console.log(x);
+
+
+if (x == undefined) {
+    console.log("x is undefined");
+}
+else {
+    console.log("x is defined");
+}
+
+x = 48;
+if (x == undefined) {
+    console.log("x is undefined");
+}
+
+else {
+    console.log("x is defined");
+}
+
+if (y == undefined) {
+    console.log("y is undefined");
+}
+else {
+    console.log("y is not defined");
+}
+
